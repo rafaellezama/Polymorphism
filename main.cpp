ceddbd01b160b9c7f660a1f4c6e85995aaa05808
rafaellezama <lr43469@mail.harpercollege.edu>
2024-10-08 09:57:46 -0500

    w
    w

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "Rectangle.hpp"
 
-int main(){
-  Shape* rect = new Rectangle(5,5);
+int main()
+{
+  Shape *rect = new Rectangle(5, 5);
   cout << "Rectangle Area: " << rect->getArea() << endl;
+
   return 0;
 }
