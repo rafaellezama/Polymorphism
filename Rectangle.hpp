3528a326458781073b19c42a92d7bb0c817e87d5
rafaellezama <lr43469@mail.harpercollege.edu>
2024-10-08 14:34:33 -0500

    rect hpp

--- Rectangle.hpp
+++ Rectangle.hpp
@@ -1,17 +1,20 @@
 #include "Shape.hpp"
 
-class Rectangle : public Shape{
-  public:
-    Rectangle(double width, double height);
-    double getArea() const override{
-      return width * height;
-    }
+class Rectangle : public Shape
+{
+public:
+  Rectangle(double width, double height);
+  double getArea() const override
+  {
+    return width * height;
+  }
 
-    double getPerimeter() const override{
-      return 2 * (width + height);
-    }
+  double getPerimeter() const override
+  {
+    return 2 * (width + height);
+  }
 
-  private: 
-    double width;
-    double height;
+private:
+  double width;
+  double height;
 };
